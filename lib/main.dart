33f447e6d8158eacd507a98f6fc28fced59c7d1f
tensor-programming <abeltensor@tensor-programming.com>
2018-12-21 17:12:03 -0500

    "revert"
    This reverts commit 3f327e5c6c2447fe99b73d1730159467e5f0d230.

--- lib/main.dart
+++ lib/main.dart
@@ -1,31 +1,42 @@
 import 'package:flutter/material.dart';
 import 'package:package_info/package_info.dart';
-import 'package:utopian_rocks_mobile/blocs/contribution_bloc.dart';
-import 'package:utopian_rocks_mobile/blocs/information_bloc.dart';
-import 'package:utopian_rocks_mobile/components/information_drawer.dart';
-import 'package:utopian_rocks_mobile/components/list_page.dart';
-import 'package:utopian_rocks_mobile/models/github_api.dart';
-import 'package:utopian_rocks_mobile/models/rocks_api.dart';
-import 'package:utopian_rocks_mobile/components/bottom_bar.dart';
 
-import 'package:utopian_rocks_mobile/blocs/steem_bloc.dart';
-import 'package:utopian_rocks_mobile/models/steem_api.dart';
+import 'package:utopian_rocks/blocs/contribution_bloc.dart';
+import 'package:utopian_rocks/blocs/information_bloc.dart';
+import 'package:utopian_rocks/components/bottom_sheet.dart';
+import 'package:utopian_rocks/components/drawer.dart';
+import 'package:utopian_rocks/components/list_page.dart';
+import 'package:utopian_rocks/model/github_api.dart';
+import 'package:utopian_rocks/model/repository.dart';
+import 'package:utopian_rocks/model/steem_api.dart';
+import 'package:utopian_rocks/providers/contribution_provider.dart';
+import 'package:utopian_rocks/providers/information_provider.dart';
+// import 'package:utopian_rocks/utils/utils.dart';
 
-import 'package:utopian_rocks_mobile/blocs/base_provider.dart';
+void main() {
+  // Future code for desktop-embedding.
+  // setTargetPlatformDesktop();
+  runApp(MyApp());
+}
 
-void main() => runApp(MyApp());
-
+// Root of the widget tree.
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocProvider<ContributionBloc>(
-      builder: (_, bloc) =>
-          bloc ??
-          ContributionBloc(
-            RocksApi(),
-          ),
-      onDispose: (_, bloc) => bloc.dispose(),
-      child: RootApp(),
+    // root widget is the [ContributionProvider] so that the BLoC is available anywhere.
+    return InformationProvider(
+      informationBloc: InformationBloc(
+        PackageInfo.fromPlatform(),
+        GithubApi(),
+      ),
+      child: ContributionProvider(
+        // Instantiate the API and the BLoC for the entire application.
+        contributionBloc: ContributionBloc(
+          Api(),
+          SteemApi(),
+        ),
+        child: RootApp(),
+      ),
     );
   }
 }
@@ -33,29 +44,33 @@ class MyApp extends StatelessWidget {
 class RootApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final contributionBloc = Provider.of<ContributionBloc>(context);
+    final informationBloc = InformationProvider.of(context);
+    final contributionBloc = ContributionProvider.of(context);
+
+    contributionBloc.steemApi.calculateVotingPower();
 
     return MaterialApp(
+      // Remove Debug flag to allow app to be production ready.
       debugShowCheckedModeBanner: false,
       title: 'Utopian Rocks Mobile',
+      // Setup theme data for Utopian Lato font and Color
       theme: ThemeData(
+        fontFamily: 'Lato-Regular',
         primaryColor: Color(0xff24292e),
         accentColor: Color(0xff26A69A),
       ),
+      // Setup the tab controller for the two different tabs.
       home: DefaultTabController(
         length: 2,
         child: Scaffold(
-          bottomNavigationBar: BlocProvider<SteemBloc>(
-            builder: (_, bloc) => bloc ?? SteemBloc(SteemApi()),
-            onDispose: (_, bloc) => bloc.dispose(),
-            child: BottomBar(contributionBloc),
-          ),
           appBar: AppBar(
+            // [Flex] allows the image and title to be on the same line in the [AppBar].
+            // Use [MainAxisAlignment.spaceEvenly] to add space between the two widgets.
             title: Flex(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
               crossAxisAlignment: CrossAxisAlignment.center,
               direction: Axis.horizontal,
-              children: <Widget>[
+              children: [
                 Flexible(
                   flex: 2,
                   child: Image.asset(
@@ -67,6 +82,8 @@ class RootApp extends StatelessWidget {
                 ),
               ],
             ),
+
+            // Add [TabBar] to the [AppBar] to allow the user to navigate from one page to the next.
             bottom: TabBar(
               tabs: <Widget>[
                 Tab(
@@ -80,22 +97,29 @@ class RootApp extends StatelessWidget {
               ],
             ),
           ),
+          // Body of the application which points towards the two pages.
+          // Page one lists the unreviewed contributions and Page two lists the pending contributions.
           body: TabBarView(
             children: <Widget>[
-              ListPage('unreviewed', contributionBloc),
-              ListPage('pending', contributionBloc),
+              ListPage('unreviewed', contributionBloc, callback),
+              ListPage('pending', contributionBloc, callback),
             ],
           ),
-          endDrawer: BlocProvider<InformationBloc>(
-            builder: (_, bloc) => InformationBloc(
-                  PackageInfo.fromPlatform(),
-                  GithubApi(),
-                ),
-            onDispose: (_, bloc) => bloc.dispose(),
-            child: InformationDrawer(),
-          ),
+          // add [BottomSheetbar] to the [Scaffold]
+          bottomNavigationBar: BottomSheetbar(contributionBloc),
+          // Add the [InformationDrawer] to the [Scaffold]
+          endDrawer: StreamBuilder(
+              stream: informationBloc.infoStream,
+              builder: (context, snapshot) {
+                return InformationDrawer(snapshot);
+              }),
         ),
       ),
     );
+  }
+
+  // Callback function which inserts tabName into the TabName [Sink]
+  void callback(String tabName, ContributionBloc bloc) {
+    bloc.tabName.add(tabName);
   }
 }
